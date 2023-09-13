3fcdf3283375c769b265b04b2dc57208e8a84e8d
monocathaver <kirbybryan.oledan@gmail.com>
2023-09-13 23:02:45 +0800

    Adi ngayan an ERD forreal na this time

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
 <h1>Welcome to SvelteKit</h1>
+
+<script>
+	let name = '';
+</script>
+
+<input bind:value={name} placeholder="enter your name" />
+<p>Hello {name || 'stranger'}!</p>
+
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
