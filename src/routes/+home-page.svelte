53d654e08bf66021b6c1cb1a30c85d89c839aeb5
monocathaver <kirbybryan.oledan@gmail.com>
2023-09-28 14:46:03 +0800

    Trial 1

---
+++ src/routes/+home-page.svelte
@@ -0,0 +1,19 @@
+<title>Home Page</title>
+
+<script>
+	let user_email = '';
+</script>
+
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<form method="POST">
+	<label>
+		Email
+		<input name="email" type="email">
+	</label>
+	<label>
+		Password  
+		<input name="password" type="password">
+	</label>
+	<button>Log in</button>
+</form>
